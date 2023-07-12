566d04c257d081f145ed18a632e3086acf46bcdb
contactharrisc2 <contactharrisc2@gmail.com>
2023-07-12 05:00:03 -0500

    adds routes that can send files to browser client

--- controllers/SiteController.js
+++ controllers/SiteController.js
@@ -3,6 +3,8 @@
 /////////////////////
 require("dotenv").config();
 const express = require("express");
+const path = require("path");
+const { nextTick } = require("process");
 
 //////////////////
 // Create router application
@@ -10,3 +12,44 @@ const express = require("express");
 const router = express.Router();
 
 module.exports = router;
+
+//////////////////
+// ROUTES
+//////////////////
+
+//root:
+router.get("/", (req, res)=>{
+    const options = {
+        root: path.join(__dirname, "../views")
+    };
+
+    const fileName = "index.html";
+    res.sendFile(fileName, options, (err)=>{
+        if(err) {
+            console.log(err);
+        }else{
+            console.log('Sent: ', fileName);
+        }
+    });
+});
+
+//user
+router.get("/user", (req, res)=>{
+    const options = {
+        root: path.join(__dirname, "../views")
+    };
+
+    const fileName = "forms.html"
+    res.sendFile(fileName, options, (err)=>{
+        if(err){
+            console.log(err);
+        }else{
+            console.log("Sent: ", fileName);
+        }
+    });
+});
+
+router.post("/user/form", (req, res) =>{
+    console.log(req.body);
+    res.send("Sent");
+})
